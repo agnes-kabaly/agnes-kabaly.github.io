f7a59b9d1efd72b1acd27fb6899264463243387c
agnes-kabaly <alicet.woods@gmail.com>
2018-10-17 15:16:29 +0200

    path correction

--- src/webapp/js/index.js
+++ src/webapp/js/index.js
@@ -11,7 +11,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = '../assets/dice-' + dice + '.png';
+        diceDOM.src = 'src/assets/dice-' + dice + '.png';
 
 
         //3. Update the round score IF the rolled number was NOT a 1
